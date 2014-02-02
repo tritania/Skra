70c89cb6ad5db8677480e0e74357225d9d830f87
Erik Wilson <erikwilson@magnorum.com>
2014-02-02 15:26:57 -0400

    Began implementing multiple graphs, added menu scrollbar for people below 1080p, continued work on menu options.

--- js/drawgraph.js
+++ js/drawgraph.js
@@ -1,7 +1,27 @@
+function drawgraph(x)
+{
+	switch(x)
+	{
+		case 1:
+			{
+			drawlinegraph();
+			break;
+			}
+			
+		case 2:
+			{
+			drawpiechart();
+			break;
+			}
+	}
+}
+
+
+
 function drawlinegraph()
 {
     var margin = {top: 20, right: 20, bottom: 30, left: 50},
-        width = (screen.availWidth*0.78) - margin.left - margin.right,
+        width = (screen.availWidth-352) - margin.left - margin.right,
         height = window.innerHeight - margin.top - margin.bottom - 100;
 
     var parseDate = d3.time.format("%d-%b-%y").parse;
@@ -50,7 +70,7 @@ function drawlinegraph()
         .append("text")
           .attr("transform", "rotate(-90)")
           .attr("y", 6)
-          .attr("dy", ".71em")
+          .attr("dy", ".85em")
           .style("text-anchor", "end")
           .text("Blood Sugar");
 
@@ -59,4 +79,51 @@ function drawlinegraph()
           .attr("class", "line")
           .attr("d", line);
     });
+}
+
+function drawpiechart()
+{
+	var width = 960,
+    height = 500,
+    radius = Math.min(width, height) / 2;
+
+var color = d3.scale.ordinal()
+    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+
+var arc = d3.svg.arc()
+    .outerRadius(radius - 10)
+    .innerRadius(0);
+
+var pie = d3.layout.pie()
+    .sort(null)
+    .value(function(d) { return d.population; });
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+  .append("g")
+    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+d3.csv("data/data.csv", function(error, data) {
+
+  data.forEach(function(d) {
+    d.population = +d.population;
+  });
+
+  var g = svg.selectAll(".arc")
+      .data(pie(data))
+    .enter().append("g")
+      .attr("class", "arc");
+
+  g.append("path")
+      .attr("d", arc)
+      .style("fill", function(d) { return color(d.data.age); });
+
+  g.append("text")
+      .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+      .attr("dy", ".85em")
+      .style("text-anchor", "middle")
+      .text(function(d) { return d.data.age; });
+
+});
 }
