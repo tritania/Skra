e4c2d33e053bf31e637ffe4936a49a5faf4e6ae1
Tritania <erikwilson@magnorum.com>
2014-02-11 23:21:57 -0400

    Removed password hashing to allow for end user development ease, and to allow a security redesign.

--- php/login.php
+++ php/login.php
@@ -3,9 +3,6 @@ $password = $_GET['password'];
 $username = $_GET['username'];
 $mysqli = new mysqli("localhost", "SKRA", "PASSWORD", "LOGINDATA");
 
-$key = 'PHP_SUCKS';
-$cipher = "MCRYPT_SERPENT_256";
-$mode = "MCRYPT_MODE_CBC";
 
 
 if ($mysqli->connect_errno) {
@@ -16,28 +13,16 @@ if ($mysqli->connect_errno) {
 if ($result = $mysqli->query("SELECT PASSWORD FROM LOGIN WHERE USERNAME ='" . $username ."'")) 
 {
     $row = mysqli_fetch_row($result);
-    $decrypted_password = m_decrypt($row[0], $key, $cipher, $mode);
-    if ($decrypted_password == $row[0]) 
+    if ($password == $row[0]) 
     {
 		echo '1'; //valid
 	} 
 	else 
 	{
 		echo '2'; //invalid
-		echo $decrypted_password;
 	}
 
     $result->close();
 }
 $mysqli->close();
-function m_decrypt($data, $key, $cipher, $mode){
- return (string)
-   mcrypt_decrypt(
-    $cipher,
-    substr(md5($key),0,mcrypt_get_key_size($cipher, $mode)),
-    base64_decode($data),
-    $mode,
-    substr(md5($key),0,mcrypt_get_block_size($cipher, $mode))
-   );
-}
 ?>
--- php/register.php
+++ php/register.php
@@ -7,23 +7,6 @@ $weight = $_GET['weight'];
 $height = $_GET['height'];
 $age = $_GET['age'];
 
-$key = 'PHP_SUCKS';
-$cipher = "MCRYPT_SERPENT_256";
-$mode = "MCRYPT_MODE_CBC";
-
-function m_encrypt($password, $key, $cipher, $mode){
- return (string) 
-  base64_encode(
-   mcrypt_encrypt(
-    $cipher,
-    substr(md5($key),0,mcrypt_get_key_size($cipher, $mode)),
-    $data,
-    $mode,
-    substr(md5($key),0,mcrypt_get_block_size($cipher, $mode))
-   )
-  );
-}
-$newpass = m_encrypt($password, $key, $cipher, $mode);
 $mysqli = new mysqli("localhost", "SKRA", "PASSWORD", "LOGINDATA");
 
 if ($mysqli->connect_errno) 
@@ -33,7 +16,7 @@ if ($mysqli->connect_errno)
 }
 
 if ($result = $mysqli->query("INSERT INTO USERINFO VALUES ('". $email ."' , '". $name ."' , '". $weight ."' , '". $height ."' , '". $age . "')"));
-if ($result = $mysqli->query("INSERT INTO LOGIN VALUES ('". $username ."' , '". $newpass ."')"));
+if ($result = $mysqli->query("INSERT INTO LOGIN VALUES ('". $username ."' , '". $password ."')"));
 $mysqli->close();
 
 ?>
