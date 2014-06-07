1fb0734598cb188a6f80ba9d0531fa51710dda1f
Tritania <erikwilson@magnorum.com>
2014-06-07 18:49:24 -0300

    Register button styling, added more functionality to the registry system.

--- js/register.js
+++ js/register.js
@@ -3,3 +3,9 @@ function signup() {
     d3.select(".registerdiv").style("visibility", "visible");
     d3.select(".prc").attr("id", "registercover");
 }
+
+function closereg() {
+    "user strict";
+    d3.select(".registerdiv").style("visibility", "hidden");
+    d3.select(".prc").attr("id", "nonactive");
+}
