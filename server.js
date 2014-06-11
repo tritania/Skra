47bdbb556c435e996e65c11c637ed2b6c9c01984
Tritania <erikwilson@magnorum.com>
2014-06-11 03:34:54 -0300

    Added express framework to the project.

--- server.js
+++ server.js
@@ -1,34 +1,20 @@
-var http = require("http"),
-    url = require("url"),
-    path = require("path"),
-    fs = require("fs")
- 
-var app = http.createServer(function(request, response) {
- 
-  var uri = url.parse(request.url).pathname
-    , filename = path.join(process.cwd(), uri);
-  
-  path.exists(filename, function(exists) {
-    if(!exists) {
-      response.writeHead(404, {"Content-Type": "text/plain"});
-      response.write("404 Not Found\n");
-      response.end();
-      return;
-    }
- 
-    if (fs.statSync(filename).isDirectory()) filename += '/index.html';
- 
-    fs.readFile(filename, "binary", function(err, file) {
-      if(err) {        
-        response.writeHead(500, {"Content-Type": "text/plain"});
-        response.write(err + "\n");
-        response.end();
-        return;
-      }
- 
-      response.writeHead(200);
-      response.write(file, "binary");
-      response.end();
-    });
-  });
-}).listen(80);
+var express = require("express"),
+    app = express(),
+    methodOverride = require('method-override'),
+    bodyParser = require('body-parser'),
+    errorHandler = require('errorhandler'),
+    port = 4567;
+
+app.get("/", function (req, res) {
+  res.redirect("/index.html");
+});
+
+app.use(methodOverride());
+app.use(bodyParser());
+app.use(express.static(__dirname + '/public'));
+app.use(errorHandler({
+  dumpExceptions: true,
+  showStack: true
+}));
+
+app.listen(port);
