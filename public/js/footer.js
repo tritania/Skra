4487ec0797be405a1a57de5e88e040956e85f31b
Tritania <erikwilson@magnorum.com>
2014-06-29 02:06:49 +0000

    Updated fonts, footer.js dismiss function.

--- public/js/footer.js
+++ public/js/footer.js
@@ -3,4 +3,11 @@
 function showFooterElement(element) {
     "use strict";
     d3.select(element).style("visibility", "visible");
+    d3.select(".prc").attr("id", "registercover");
+}
+
+function dismissFooterElement(element) {
+    "use strict";
+    d3.select(element).style("visibility", "hidden");
+    d3.select(".prc").attr("id", "nonactive");
 }
