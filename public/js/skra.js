6a2bb53907f46b3f3df7303c12beaf3cb0070576
Erik wilson <erikwilson@magnorum.com>
2015-11-23 01:08:14 -0400

    Further strip down of repo, more work on login form.

---
+++ public/js/skra.js
@@ -0,0 +1,11 @@
+/* Account Login and Signup Functions*/
+
+function loginform() {
+    "use strict";
+    document.getElementById("entry").style.visibility = "visible";
+}
+
+function closeform() {
+    "use strict";
+    document.getElementById("entry").style.visibility = "hidden";
+}
